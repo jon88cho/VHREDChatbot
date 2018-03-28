19b11ed3772e63f6d84b1a6dfac72ec30386f4da
Jonathan Cho <jon88cho@gmail.com>
2018-03-27 20:21:17 -0400

    processing.py added list of characters as return value

--- processing.py
+++ processing.py
@@ -11,25 +11,40 @@ def switch_key_value (dictionary):
     return dictionary
 
 #character level
-def string_to_character_embeddings(filename):
-        file = open(filename, 'r')
-        raw_text = file.read()
-        file.close()
-        words = raw_text.split()
-        tokens = ' '.join(words)
-        lowercased = [token.lower() for token in tokens]
-        chars = sorted(list(set(lowercased)))
-        chars.append("</s>")
-        mapping = dict((c, i) for i, c in enumerate(chars))
-        size = len(mapping)
-        for i in range(size):
-            zeros = np.zeros(size)
-            zeros[i] = 1
-            one_hot_vector = zeros #[np.newaxis].T  uncomment if the shape is (n,1)
-            key = list(mapping.keys())[i]
-            mapping[key] = tf.convert_to_tensor(one_hot_vector)
+def string_to_character_embeddings(sequence_length, filename):
+    file = open(filename, 'r')
+    raw_text = file.read()
+    file.close()
+    chars = []
+    for char in raw_text:
+        chars.append(char)
+    counter = 0
+    list_of_chars = []
+    for i in range(int(len(chars) / sequence_length)):
+        tempcharlist = chars[i * sequence_length:(sequence_length) * (i + 1)]
+        tempcharlist.append("</s>")
+        list_of_chars.append(tempcharlist)
 
-        return mapping
+        counter = i
+    remainder = len(chars) % sequence_length
+    remainderlist = chars[((sequence_length) * (counter + 1)):((sequence_length) * (counter + 1) + remainder)]
+    remainderlist.append("</s>")
+    list_of_chars.append(remainderlist)
+    words = raw_text.split()
+    tokens = ' '.join(words)
+    lowercased = [token.lower() for token in tokens]
+    chars = sorted(list(set(lowercased)))
+    chars.append("</s>")
+    mapping = dict((c, i) for i, c in enumerate(chars))
+    size = len(mapping)
+    for i in range(size):
+        zeros = np.zeros(size)
+        zeros[i] = 1
+        one_hot_vector = zeros  # [np.newaxis].T  uncomment if the shape is (n,1)
+        key = list(mapping.keys())[i]
+        mapping[key] = (one_hot_vector)
+
+    return list_of_chars, mapping
 
 
 # corpus is a list of strings
@@ -48,10 +63,10 @@ def word_embeddings_mapping(corpus):
 
 
 #regular dictionary
-string_to_charcater_dictionary = string_to_character_embeddings("test.txt")
+list_of_characters, dictionary = string_to_charcater_dictionary = string_to_character_embeddings("test.txt")
 print(string_to_charcater_dictionary)
 # Print the "tuple as the keys" dictionary
-character_to_string_dictionary = switch_key_value(string_to_charcater_dictionary)
+character_to_string_dictionary = switch_key_value(dictionary)
 print(character_to_string_dictionary)
 
 #regular word embedding dictionary
